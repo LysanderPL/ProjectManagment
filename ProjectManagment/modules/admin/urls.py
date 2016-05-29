09d1de1269fd606d9b4773e83e155378fcd38920
LysanderPL <podania.12@gmail.com>
2016-05-29 21:14:13 +0200

    Cleaning garbages

--- ProjectManagment/modules/admin/urls.py
+++ ProjectManagment/modules/admin/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
 
 from ProjectManagment.modules.admin.views.index import Index
-from ProjectManagment.modules.admin.views.rekt import Rekt
 
 
 def getModuleUrls():
     return [
-        url(r'^$', Index.as_view(), name='index'),
-        url(r'^rekt/$', Rekt.as_view(), name="rekt")
+        url(r'^$', Index.as_view(), name='index')
     ]
